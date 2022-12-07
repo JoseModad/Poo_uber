5d4318b54fc030715c7fea89eeacde682e73d2b3
turko2373 <turko1073@gmail.com>
2022-12-07 17:43:45 -0300

    terminando clases

--- account.py
+++ account.py
@@ -9,3 +9,6 @@ class Account:
     def __init__(self, name, document):
         self.name = name
         self.document = document
+        
+       
+        
---
+++ uberBlack.py
@@ -0,0 +1,12 @@
+from car import Car
+
+
+class UberBlack(Car):
+    typeCarAccepted = []
+    seatMaterial = []
+    
+    
+    def __init__(self, license, driver, typeCarAccepted, seatMaterial):
+        super().__init__(license, driver)
+        self.tytypeCarAccepted = typeCarAccepted
+        self.seatMseatMaterial = seatMaterial
