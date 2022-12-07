261acdaf56e8998e2f5647af0233f99a84d64bfd
turko2373 <turko1073@gmail.com>
2022-12-07 14:43:41 -0300

    Probando funcionamiento class Account y Car

--- car.py
+++ car.py
@@ -1,6 +1,13 @@
+from account import Account
+
+
 class Car:
     id = int
     license = str
-    driver = str
-    passeger = int
+    driver = Account('', '')
+    passenger = int
     
+    
+    def __init__(self, license, driver):
+        self.license = license
+        self.driver = driver
--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+from car import Car
+from account import Account
+
 if __name__ == '__main__':
-    print('Hola mundo')
+    
+    car = Car('KJH897', Account('Marcos Torres', 'PKI347'))   
+    print(vars(car))
+    print(vars(car.driver))
+    
+    car2 = Car('MKL372', Account('Antonio Rios', 'MKL372'))    
+    print(vars(car2))
+    print(vars(car2.driver))
+  
